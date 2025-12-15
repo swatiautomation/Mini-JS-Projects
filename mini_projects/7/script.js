67a490a3273b84e0562249765714e0da64438c18
swatiautomation <chinki.sharma73@gmail.com>
2025-12-15 20:42:30 +0000

    Rock paper scissor project auto play added

--- mini_projects/7/script.js
+++ mini_projects/7/script.js
@@ -5,19 +5,15 @@ const resetButton = document.querySelector('.reset-button');
 const computerMoveDiv = document.querySelector('.computer-move');
 const resultMessageDiv = document.querySelector('.result-message');
 const scoreDiv = document.querySelector('.score');
+const autoPlayButton = document.querySelector('.auto-play-button');
+const confirmationDialog = document.querySelector('.confirmation-dialog');
 
 rock.addEventListener('click', () => play('rock'));
 paper.addEventListener('click', () => play('paper'));
 scissors.addEventListener('click', () => play('scissors'));
-
+autoPlayButton.addEventListener('click', () => autoPlay());
 resetButton.addEventListener('click', () => {
-  score.wins = 0;
-  score.losses = 0;
-  score.ties = 0;
-  localStorage.removeItem('score');
-  updateScore();
-  computerMoveDiv.innerText = '';
-  resultMessageDiv.innerText = '';
+  showResetConfirmation();
 });
 
 let score = JSON.parse(localStorage.getItem('score')) || {
@@ -26,6 +22,16 @@ let score = JSON.parse(localStorage.getItem('score')) || {
   ties: 0,
 };
 
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScore();
+  computerMoveDiv.innerText = '';
+  resultMessageDiv.innerText = '';
+  confirmationDialog.innerHTML = '';
+}
 function updateScore() {
   scoreDiv.innerText = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
   scoreDiv.style.color = '#ebe3e3';
@@ -48,7 +54,7 @@ function showResult(message, color) {
 
 function play(playerMove) {
   const move = ['rock', 'paper', 'scissors'];
-  const computerMove = move[Math.floor(Math.random() * 3)];
+  const computerMove = move[Math.floor(Math.random() * move.length)];
 
   computerMoveDiv.innerHTML = `You
       <img src="./images/${playerMove}-emoji.png" class="move-icon" />
@@ -66,5 +72,52 @@ function play(playerMove) {
     showResult('You win!', 'green');
   } else {
     showResult('You loose!', 'red');
+  }
+}
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    play('rock');
+  } else if (event.key === 'p') {
+    play('paper');
+  } else if (event.key === 's') {
+    play('scissors');
+  } else if (event.key === 'a') {
+    autoPlay();
+  } else if (event.key === 'Backspace') {
+    showResetConfirmation();
+  }
+});
+function showResetConfirmation() {
+  confirmationDialog.innerHTML = `<div> Are you sure you want to reset the score?</div>
+    <button class="yes-button"> Yes </button>
+    <button class="no-button"> No </button>`;
+
+  const yesButton = document.querySelector('.yes-button');
+  const noButton = document.querySelector('.no-button');
+  yesButton.addEventListener('click', () => {
+    resetScore();
+  });
+
+  noButton.addEventListener('click', () => {
+    confirmationDialog.innerHTML = '';
+  });
+}
+
+let isAutoPlaying = false;
+let intervalId;
+function autoPlay() {
+  if (!isAutoPlaying) {
+    const moves = ['rock', 'paper', 'scissors'];
+    intervalId = setInterval(() => {
+      const randomMove = moves[Math.floor(Math.random() * moves.length)];
+      play(randomMove);
+    }, 1000);
+    isAutoPlaying = true;
+    autoPlayButton.innerText = 'Stop Playing';
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    autoPlayButton.innerText = 'Auto Play';
   }
 }
