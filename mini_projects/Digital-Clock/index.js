df93e5962f309e4245e5c17f7c1ff209511aeced
swatiautomation <chinki.sharma73@gmail.com>
2025-12-31 21:55:35 +0000

    Digital clock project code updated

--- mini_projects/Digital-Clock/index.js
+++ mini_projects/Digital-Clock/index.js
@@ -1,16 +1,21 @@
 const digitalClock = document.querySelector('.digitalClock');
+const hr = document.querySelector('#hours');
+const min = document.querySelector('#minutes');
+const timeFormate = document.querySelector('#ampm');
+const day = document.querySelector('#day');
 
-window.addEventListener('load', updateClock);
+window.addEventListener('load', () => {
+  setInterval(updateClock, 1000);
+});
 
 function updateClock() {
   let date = new Date();
+
   const dayNum = date.getDay();
-  console.log(dayNum);
   let hours = date.getHours();
-  console.log(hours);
   let minutes = date.getMinutes();
-  console.log(minutes);
-  const ampm = hours > 12 ? 'PM' : 'AM';
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
   const dayName = [
     'Sunday',
     'Monday',
@@ -20,18 +25,14 @@ function updateClock() {
     'Friday',
     'Saturday',
   ];
-  const seconds = date.getSeconds();
-  console.log(seconds);
 
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   hours = hours < 10 ? '0' + hours : hours;
   minutes = minutes < 10 ? '0' + minutes : minutes;
-  document.querySelector('#day').innerHTML = dayName[dayNum];
-  document.querySelector('#hours').innerHTML = hours;
-  document.querySelector('#minutes').innerHTML = minutes;
-  document.querySelector('#ampm').innerHTML = ampm;
 
-  setTimeout(updateClock, 2000);
+  day.innerHTML = dayName[dayNum];
+  hr.innerHTML = hours;
+  min.innerHTML = minutes;
+  timeFormate.innerHTML = ampm;
 }
-updateClock();
