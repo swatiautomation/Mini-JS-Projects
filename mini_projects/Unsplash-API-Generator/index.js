a5a36b39a26f5765147720e4e8280d3d116aba4a
swatiautomation <chinki.sharma73@gmail.com>
2026-01-02 22:45:27 +0000

    Unsplash API - load button added

--- mini_projects/Unsplash-API-Generator/index.js
+++ mini_projects/Unsplash-API-Generator/index.js
@@ -1,40 +1,63 @@
 const input = document.querySelector('#input');
 const searchBtn = document.querySelector('#search-btn');
 const grid = document.querySelector('.grid');
+const loadMoreBtn = document.querySelector('.loadMoreBtn');
+let pageNo = 1;
+
 window.addEventListener('load', dayNightMode);
+
 input.addEventListener('keyup', function (event) {
   if (event.key === 'Enter') {
-    loadImg();
+    loadImg(input.value.trim(), pageNo);
   }
 });
 
-function loadImg() {
-  removeImages();
+async function loadImg(word, pageNo) {
+  if (pageNo === 1) removeImages();
 
-  const url = `https://api.unsplash.com/search/photos?query=${input.value}&per_page=30&client_id=BekaH84Ex6BqFGpDpfV1TUvNJoNxwu32YKIifMcp5Ok`;
+  const url = `https://api.unsplash.com/search/photos?query=${word}&per_page=30&page=${pageNo}&client_id=BekaH84Ex6BqFGpDpfV1TUvNJoNxwu32YKIifMcp5Ok`;
 
-  fetch(url)
+  await fetch(url)
     .then((response) => {
-      if (response.ok) return response.json();
-      else throw new Error('Error in fetching images');
+      if (response.ok) {
+        return response.json();
+      } else throw new Error('Error in fetching images');
     })
-
     .then((data) => {
-      const imageNodes = [];
+      console.log(data);
 
-      for (let i = 0; i < data.results.length; i++) {
-        imageNodes[i] = document.createElement('div');
-        imageNodes[i].className = 'img';
-        imageNodes[
-          i
-        ].style.backgroundImage = `url(${data.results[i].urls.raw})`;
-        imageNodes[i].addEventListener('dblclick', () => {
-          window.open(data.results[i].links.download, '_blank');
+      if (data.results.length > 0) {
+        data.results.forEach((photo) => {
+          const imgDiv = document.createElement('div');
+          imgDiv.className = 'img';
+          imgDiv.style.backgroundImage = `url(${photo.urls.regular})`;
+
+          const tooltip = document.createElement('div');
+          tooltip.classList.add('tooltip');
+          tooltip.innerText = `${photo.alt_description}`;
+          imgDiv.appendChild(tooltip);
+
+          imgDiv.addEventListener('dblclick', () => {
+            window.open(photo.links.download, '_blank');
+          });
+          grid.appendChild(imgDiv);
         });
+        if (data.total_pages === pageNo) {
+          loadMoreBtn.style.display = 'none';
+        } else {
+          loadMoreBtn.style.display = 'block';
+        }
+      } else {
+        grid.innerHTML = `<h2>No results found</h2>`;
+        loadMoreBtn.style.display = 'none';
       }
-      grid.append(...imageNodes);
     });
 }
+
+loadMoreBtn.addEventListener('click', () => {
+  pageNo++;
+  loadImg(input.value.trim(), pageNo);
+});
 
 function removeImages() {
   grid.innerHTML = '';
