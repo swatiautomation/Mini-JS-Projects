61975c3c525dd0b79a8ea01b5a60f1ebc99d4967
swatiautomation <chinki.sharma73@gmail.com>
2026-01-04 15:26:36 +0000

    Digital clock app updated

--- mini_projects/Digital-Clock/index.js
+++ mini_projects/Digital-Clock/index.js
@@ -28,8 +28,10 @@ function updateClock() {
 
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  hours = hours < 10 ? '0' + hours : hours;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
+  // hours = hours < 10 ? '0' + hours : hours;
+  hours = hours.toString().padStart(2, '0');
+  //minutes = minutes < 10 ? '0' + minutes : minutes;
+  minutes = minutes.toString().padStart(2, '0');
 
   day.innerHTML = dayName[dayNum];
   hr.innerHTML = hours;
